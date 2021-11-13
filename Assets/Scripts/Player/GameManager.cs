9be2c68c06a48f8f0a5670aef70596b4b00f689c
Atheer M <43179208+atheer10@users.noreply.github.com>
2021-11-13 14:05:32 +0300

    adding particles trigger

--- Assets/Scripts/Player/GameManager.cs
+++ Assets/Scripts/Player/GameManager.cs
@@ -8,8 +8,13 @@ public class GameManager : MonoBehaviour
     Water water;
     PlayerControl player;
     Color invisablePlayer;
+    TransitionController transitioned;
+
     void Start()
     {
+        transitioned = FindObjectOfType<TransitionController>();
+        transitioned?.OnWinter.AddListener(particlesPlay);
+        transitioned?.OnSpring.AddListener(particlesPlay);
         player = FindObjectOfType<PlayerControl>();
         invisablePlayer = player.GetComponent<SpriteRenderer>().color;
         foreach (Water water in FindObjectsOfType<Water>())
@@ -31,5 +36,20 @@ public class GameManager : MonoBehaviour
         yield return new WaitForSeconds(0.2f);
         player.transform.position = lastPos;
         invisablePlayer.a = 1f;
+    }
+
+    private void particlesPlay()
+    {
+        if (transitioned.IsSpring)
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+            transform.GetChild(1).gameObject.SetActive(false);
+
+        }
+        if (transitioned.IsWinter)
+        {
+            transform.GetChild(1).gameObject.SetActive(true);
+            transform.GetChild(0).gameObject.SetActive(false);
+        }
     }
 }
